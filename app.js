e3cc19bc113916cfd86c00abec2cd13b83861c0c
Max Lewis <max.lewis@genero.com>
2017-10-04 21:37:45 +1100

    i should test stuff before i deploy

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/dist');
-  console.log('something');
+  console.log(__dirname + '/dist/index.html');
+  res.sendFile(__dirname + '/dist/index.html');
 });
 
-app.listen(80, function () {
+app.use(express.static('dist'));
+
+app.listen(8080, function () {
   console.log('listening');
 });
