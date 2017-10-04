abc68489d8818efcdb0992527caae3cb077d16ef
Max Lewis <max.lewis@genero.com>
2017-10-05 10:13:58 +1100

    adding the keyboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NgModuleFactoryLoader, SystemJsNgModuleLoader } from '@angular/core';
+import { UIRouterModule, UIView } from '@uirouter/angular';
 
+import { APP_STATES } from './app.states';
 import { AppComponent } from './app.component';
+import { KeyboardComponent } from './keyboard/keyboard.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KeyboardComponent,
+    HomeComponent
   ],
   imports: [
+    UIRouterModule.forRoot({
+      states: APP_STATES,
+      useHash: true,
+      otherwise: { state: 'app' },
+    }),
     BrowserModule
   ],
   providers: [],
