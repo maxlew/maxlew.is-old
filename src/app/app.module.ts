771e16866ff5af78e62f9b997d05c71f8d8bdad4
Max Lewis <max.lewis@genero.com>
2017-10-06 08:57:10 +1100

    remove files and coode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// import { UIRouterModule, UIView } from '@uirouter/angular';
 
-// import { APP_STATES } from './app.states';
 import { AppComponent } from './app.component';
 import { KeyboardComponent } from './keyboard/keyboard.component';
 import { HomeComponent } from './home/home.component';
@@ -14,11 +12,6 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
-    // UIRouterModule.forRoot({
-    //   states: APP_STATES,
-    //   useHash: true,
-    //   otherwise: { state: 'app' },
-    // }),
     BrowserModule
   ],
   providers: [],
