8862720c8682c54f7a6d4fbd76163a9388c61c42
Max Lewis <max.lewis@genero.com>
2017-10-15 19:38:29 +1100

    fix keyboard bindings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+
+
 
 import { AppComponent } from './app.component';
 import { KeyboardComponent } from './keyboard/keyboard.component';
