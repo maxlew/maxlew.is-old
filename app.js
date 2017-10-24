1a5dec6ff008e63451bffe44bc5d7fb08159f1e4
Max Lewis <max.lewis@genero.com>
2017-10-25 09:49:48 +1100

    switching ports as apache is going to handle more now

--- app.js
+++ app.js
@@ -5,20 +5,15 @@ var https = require('https');
 var fs = require('fs');
 var express = require('express');
 var helmet = require('helmet');
-var httpProxy = require('http-proxy');
 var app = module.exports.app = express();
 
-// Mail server setup
-var apiProxy = httpProxy.createProxyServer();
-var mailPileServer = 'http://localhost:33411'
 
 var options = {
   key: fs.readFileSync('/etc/letsencrypt/live/maxlew.is/privkey.pem'),
   cert: fs.readFileSync('/etc/letsencrypt/live/maxlew.is/fullchain.pem')
 };
 
-var secureServer = https.createServer(options, app).listen(443);
-var server = http.createServer(app).listen(80);
+var secureServer = https.createServer(options, app).listen(8443);
 
 var io = require('socket.io').listen(secureServer);
 var compression = require('compression')
@@ -26,13 +21,7 @@ var compression = require('compression')
 app.use(compression());
 app.use(helmet());
 
-app.all('*', ensureHTTPS)
-
 app.get('*', function (req, res, next) {
-  // uri has a forward slash followed any number of any characters except fjsull stops (up until the end of the string)
-  if (req.url == '/mail/') {
-    apiProxy.web(req, res, { target: mailPileServer });
-  }
   if (/\/[^.]*$/.test(req.url)) {
       res.sendFile(__dirname + '/dist/index.html');
   } else {
@@ -41,13 +30,6 @@ app.get('*', function (req, res, next) {
 });
 
 app.use(express.static('dist'));
-
-function ensureHTTPS(req, resp, next) {
-  if (req.secure) {
-    return next();
-  }
-  resp.redirect('https://' + req.headers.host)
-}
 
 io.on('connection', function (socket) {
   socket.on('notePlayed', function (data) {
